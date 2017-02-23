010475b40b363b2cdf166cf279c5e7a699a3e96f
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-02-23 14:25:35 +0100

    lots of changes to condition and its test suite

--- lib/nl/naturalis/bioportal/Condition.php
+++ lib/nl/naturalis/bioportal/Condition.php
@@ -1,32 +1,42 @@
 <?php
     namespace nl\naturalis\bioportal;
     use nl\naturalis\bioportal\AbstractClass as AbstractClass;
+    
+    /*
+     * A condition is built from one or multiple statements. In its simplest form,
+     * a condition equals a statement. A condition can also represent a nested set of 
+     * and/or statements. Each statement is evaluated before it is appended to the condition.
+     */
 
  	final class Condition extends AbstractClass
  	{
         private $_field;
         private $_operator;
         private $_value;
-        private $_condition;
+        private $_not;
+        private $_boost;
+        private $_constantScore;
+        private $_statement = [];
+        private $_condition = [];
 
         public function __construct ($field = false, $operator = false, $value = null) {
             parent::__construct();
             $this->_bootstrapCondition($field, $operator, $value);
-            $this->_condition = $this->_setCondition();
+            $this->_setCondition();
         }
 
         public function addAnd ($fieldOrCondition, $operator = false, $value = null) {
         	// Allow setting a previously constructed Condition, cf Java client
         	if (is_object($fieldOrCondition)) {
         		if (!($fieldOrCondition instanceof Condition)) {
-        			throw new \InvalidArgumentException('Error: invalid condition.');
+        			throw new \InvalidArgumentException('Error: invalid AND condition.');
         		}
         		$this->_condition['and'][] = 
         			json_decode($fieldOrCondition->getCondition(), true);
         	// Setting an AND condition the regular way
         	} else {
 	        	$this->_bootstrapCondition($fieldOrCondition, $operator, $value);
-	            $this->_condition['and'][] = $this->_setCondition();
+	            $this->_condition['and'][] = $this->_setStatement();
         	}
         	return $this;
         }
@@ -35,58 +45,129 @@
  	    	// Allow setting a previously constructed Condition, cf Java client
  	    	if (is_object($fieldOrCondition)) {
  	    		if (!($fieldOrCondition instanceof Condition)) {
- 	    			throw new \InvalidArgumentException('Error: invalid condition.');
+ 	    			throw new \InvalidArgumentException('Error: invalid OR condition.');
  	    		}
  	    		$this->_condition['or'][] = 
  	    			json_decode($fieldOrCondition->getCondition(), true);
  	    	// Setting an OR condition the regular way
  	    	} else {
  	    	 	$this->_bootstrapCondition($fieldOrCondition, $operator, $value);
-            	$this->_condition['or'][] = $this->_setCondition();
+            	$this->_condition['or'][] = $this->_setStatement();
  	    	}
  	    	return $this;
  	    }
-
- 	    public function getCondition () {
- 	    	if (isset($this->_condition)) {
- 	    		return json_encode($this->_condition);
- 	    	}
- 	    	return false;
+ 	    
+ 	    /*
+ 	     * Can only be used to override previously initialised value
+ 	     */
+ 	    public function setField ($field = null) {
+ 	    	$this->_setField($field);
+ 	    	$this->_setCondition();
+ 	    	return $this;
  	    }
+ 	    
+ 	    /*
+ 	     * Can only be used to override previously initialised value
+ 	     */
+ 	    public function setOperator ($operator = null) {
+ 	    	$this->_setOperator($operator);
+ 	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+ 	    /*
+ 	     * Can only be used to override previously initialised value
+ 	     */
+ 	    public function setValue ($value = null) {
+ 	    	$this->_setValue($value);
+ 	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+		/**
+ 	     * Creates a negated condition if parameter equals 'not';
+ 	     * else removes negation (as per to Java client)
+ 	     */
+ 	    public function setNot ($not = 'NOT') {
+  	    	$this->_not = strtoupper($not) == 'NOT' ? 'NOT' : false;
+  	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+  	    public function setBoost ($boost = false) {
+ 	    	$boost = (float) $boost;
+ 	    	if (empty($boost)) {
+ 	    		throw new \InvalidArgumentException('Error: condition boost ' .
+ 	    			'should be a positive float.');
+ 	    	}
+ 	    	$this->_boost = $boost;
+ 	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+ 	    public function setConstantScore ($constant = true) {
+ 	    	if (!is_bool($constant)) {
+ 	    		throw new \InvalidArgumentException('Error: condition constant ' .
+ 	    			'score parameter should be TRUE (default)/FALSE.');
+ 	    	}
+ 	    	$this->_constantScore = $constant;
+ 	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+ 	    /**
+ 	     * Switches _isNot parameter (as per Java client)
+ 	     */
+ 	    public function negate () {
+ 	    	$this->_not = !$this->_not ? 'NOT' : false;
+ 	    	$this->_setCondition();
+ 	    	return $this;
+ 	    }
+ 	    
+ 	    public function getCondition () {
+ 	    	return json_encode($this->_condition);
+  	    }
  	    
  	    public function getAnd () {
  	    	if (isset($this->_condition['and'])) {
  	    		return json_encode($this->_condition['and']);
  	    	}
- 	    	return false;
+ 	    	return null;
  	    }
  	    
  	    public function getOr () {
  	    	if (isset($this->_condition['or'])) {
  	    		return json_encode($this->_condition['or']);
  	    	}
- 	    	return false;
+ 	    	return null;
  	    }
  	    
  	    public function getField () {
-        	if (isset($this->_condition['field'])) {
-        		return json_encode($this->_condition['field']);
-        	}
-        	return false;
+ 	    	return json_encode($this->_field);
         }
         
         public function getOperator () {
-        	if (isset($this->_condition['operator'])) {
-        		return json_encode($this->_condition['operator']);
-        	}
-        	return false;
+        	return json_encode($this->_operator);
         }
         
         public function getValue () {
-        	if (isset($this->_condition['value'])) {
-        		return json_encode($this->_condition['value']);
-        	}
-        	return false;
+        	return json_encode($this->_value);
+        }
+        
+        public function getBoost () {
+        	return json_encode($this->_boost);
+        }
+
+        public function getNot () {
+        	return json_encode($this->_not);
+        }
+        
+        public function isNegated () {
+        	return $this->_not == 'NOT';
+        }
+        
+        public function isConstantScore () {
+        	return $this->_constantScore;
         }
         
         private function _bootstrapCondition ($field, $operator, $value) {
@@ -114,36 +195,61 @@
          * Formats condition array; specifically excludes value if null, to allow
          * empty/not-empty queries. Should only be called after _bootstrapCondition().
          */
-        private function _setCondition () {
-        	$c['field'] = $this->_field;
-        	$c['operator'] = $this->_operator;
+        private function _setStatement () {
+        	$this->_statement['field'] = $this->_field;
+        	$this->_statement['operator'] = $this->_operator;
         	if (!is_null($this->_value)) {
-        		$c['value'] = $this->_value;
+        		$this->_statement['value'] = $this->_value;
         	}
-        	return $c;
+        	// Append secondary parameters if set; 
+        	// if set previously and now empty, unset
+        	foreach (['boost', 'constantScore', 'not'] as $p) {
+         		if ($this->{"_$p"} !== false && !is_null($this->{"_$p"})) {
+        			$this->_statement[$p] = $this->{"_$p"};
+        		} else if (isset($this->_statement[$p])) {
+        			unset($this->_statement[$p]);
+        		}
+        	}
+        	return $this->_statement;
+        }
+        
+        private function _setCondition () {
+        	// Save and/or subqueries before resetting condition
+        	$and = isset($this->_condition['and']) ? $this->_condition['and'] : false;
+        	$or = isset($this->_condition['or']) ? $this->_condition['or'] : false;
+        	// Reset condition to current statement
+        	$this->_condition = $this->_setStatement();
+        	// Reappend and/or subqueries if necessary
+        	if ($and) { 
+        		$this->_condition['and'] = $and; 
+        	}
+        	if ($or) { 
+        		$this->_condition['or'] = $or; 
+        	}
+        	return $this->_condition;
         }
         
         private function _setField ($field) {
             if (empty($field)) {
                 throw new \InvalidArgumentException('Error: condition field is not set.');
-                return false;
             }
             $this->_field = $field;
+            return $this->_field;
         }
 
  	    private function _setOperator ($operator) {
             if (empty($operator)) {
                 throw new \InvalidArgumentException('Error: condition operator is not set.');
-                return false;
             }
             if (!in_array(strtoupper($operator), self::$operators)) {
                 throw new \UnexpectedValueException('Error: ' . 
                 	'condition operator should match one of the following: ' . 
                 	implode(', ', self::$operators));
-                return false;
-            }
+             }
             $this->_operator = strtoupper($operator);
+            return $this->_operator;
         }
+        
 
  	 	/*
  	 	 * Value can be null but not an empty string
@@ -151,8 +257,9 @@
  	 	private function _setValue ($value) {
             if (empty($value) && !is_null($value)) {
                 throw new \InvalidArgumentException('Error: condition value is empty.');
-                return false;
             }
             $this->_value = $value;
+            return $this->_value;
         }
+         
  	}
--- tests/ConditionTest.php
+++ tests/ConditionTest.php
@@ -4,26 +4,23 @@ use nl\naturalis\bioportal\Condition as Condition;
 
 class ConditionTest extends \PHPUnit_Framework_TestCase
 {
-	public function testCorrectlyConstructedCondition ()
-	{
+	public function testCorrectlyConstructedCondition () {
 		$expected = '{"field":"acceptedName.genusOrMonomial",' .
 			'"operator":"EQUALS_IC","value":"larus"}';
 		$condition = new Condition('acceptedName.genusOrMonomial', 'EQUALS_IC', 'larus');
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testCorrectlyConstructedAddAndConditionWithThreeParameters ()
-	{
+	public function testCorrectlyConstructedAddAndConditionWithThreeParameters () {
 		$expected = '{"field":"acceptedName.genusOrMonomial","operator":"EQUALS_IC",' .
-				'"value":"larus","and":[{"field":"acceptedName.specificEpithet",' .
-				'"operator":"MATCHES","value":"fuscus"}]}';
+			'"value":"larus","and":[{"field":"acceptedName.specificEpithet",' .
+			'"operator":"MATCHES","value":"fuscus"}]}';
 		$condition = new Condition('acceptedName.genusOrMonomial', 'EQUALS_IC', 'larus');
 		$condition->addAnd('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testCorrectlyConstructedAddAndConditionWithCondition ()
-	{
+	public function testCorrectlyConstructedAddAndConditionWithCondition () {
 		$expected = '{"field":"acceptedName.genusOrMonomial","operator":"EQUALS_IC",' .
 				'"value":"larus","and":[{"field":"acceptedName.specificEpithet",' .
 				'"operator":"MATCHES","value":"fuscus"}]}';
@@ -33,8 +30,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testCorrectlyConstructedAddAndConditionWithIncorrectConditionAsArray ()
-	{
+	public function testCorrectlyConstructedAddAndConditionWithIncorrectConditionAsArray () {
 		$condition = new Condition('acceptedName.genusOrMonomial', 'EQUALS_IC', 'larus');
 		$condition2 = ['acceptedName.specificEpithet', 'MATCHES', 'fuscus'];
 		$e = new \stdClass();
@@ -44,8 +40,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}	
 	
-	public function testCorrectlyConstructedAddOrConditionWithThreeParameters ()
-	{
+	public function testCorrectlyConstructedAddOrConditionWithThreeParameters () {
 		$expected = '{"field":"acceptedName.genusOrMonomial","operator":"EQUALS_IC",' .
 				'"value":"larus","or":[{"field":"acceptedName.specificEpithet",' .
 				'"operator":"MATCHES","value":"fuscus"}]}';
@@ -54,8 +49,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testCorrectlyConstructedAddOrConditionWithCondition ()
-	{
+	public function testCorrectlyConstructedAddOrConditionWithCondition () {
 		$expected = '{"field":"acceptedName.genusOrMonomial","operator":"EQUALS_IC",' .
 				'"value":"larus","or":[{"field":"acceptedName.specificEpithet",' .
 				'"operator":"MATCHES","value":"fuscus"}]}';
@@ -65,8 +59,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testCorrectlyConstructedAddOrConditionWithIncorrectConditionAsArray ()
-	{
+	public function testCorrectlyConstructedAddOrConditionWithIncorrectConditionAsArray () {
 		$condition = new Condition('acceptedName.genusOrMonomial', 'EQUALS_IC', 'larus');
 		$condition2 = ['acceptedName.specificEpithet', 'MATCHES', 'fuscus'];
 		$e = new \stdClass();
@@ -76,15 +69,13 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}
 	
-	public function testConditionWithCorrectlyConstructedEmptyValue ()
-	{
+	public function testConditionWithCorrectlyConstructedEmptyValue () {
 		$expected = '{"field":"acceptedName.genusOrMonomial","operator":"NOT_EQUALS"}';
 		$condition = new Condition('acceptedName.genusOrMonomial', 'NOT_EQUALS');
 		$this->assertEquals($expected, $condition->getCondition());
 	}
 	
-	public function testConditionWithoutConstructor ()
-	{
+	public function testConditionWithoutConstructor () {
 		$e = new \stdClass();
 		try {
 			$condition = new Condition();
@@ -92,8 +83,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}
 	
-	public function testConditionWithoutField ()
-	{
+	public function testConditionWithoutField () {
 		$e = new \stdClass();
 		try {
 			$condition = new Condition(null, 'EQUALS_IC', 'larus');;
@@ -101,8 +91,7 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}
 	
-	public function testConditionWithoutOperator ()
-	{
+	public function testConditionWithoutOperator () {
 		$e = new \stdClass();
 		try {
 			$condition = new Condition('acceptedName.genusOrMonomial', null, 'larus');;
@@ -110,8 +99,78 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}
 	
-	public function testConditionWithIncorrectlyConstructedOperator ()
-	{
+	public function testConditionSetValue () {
+		$expected = json_encode('argentatus');
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition->setValue('argentatus');
+		$this->assertEquals($expected, $condition->getValue());
+	}
+	
+	public function testConditionSetField () {
+		$expected = json_encode('acceptedName.genusOrMonomial');
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'larus');
+		$condition->setField('acceptedName.genusOrMonomial');
+		$this->assertEquals($expected, $condition->getField());
+	}
+	
+	public function testConditionSetOperator () {
+		$expected = json_encode('EQUALS');
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition->setOperator('EQUALS');
+		$this->assertEquals($expected, $condition->getOperator());
+	}
+	
+	public function testResetCompleteCondition () {
+		$expected = '{"field":"acceptedName.genusOrMonomial",' .
+			'"operator":"EQUALS_IC","value":"larus"}';
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition
+			->setField('acceptedName.genusOrMonomial')
+			->setOperator('EQUALS_IC')
+			->setValue('larus');
+		$this->assertEquals($expected, $condition->getCondition());
+	}
+	
+	public function testCompleteConditionSetWithBoostConstantScoreAndNot () {
+		$expected = '{"field":"acceptedName.specificEpithet",' .
+				'"operator":"MATCHES","value":"fuscus","not":"NOT","boost":1.3,' .
+				'"constantScore":true}';
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition
+			->setNot()
+			->setBoost(1.3)
+			->setConstantScore();
+		$this->assertEquals($expected, $condition->getCondition());
+	}
+	
+	public function testConditionNegate () {
+		$expected = '{"field":"acceptedName.specificEpithet",' .
+				'"operator":"MATCHES","value":"fuscus","boost":1.3,' .
+				'"constantScore":true}';
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition
+			->setNot()
+			->setBoost(1.3)
+			->setConstantScore();
+		$condition->negate();
+		$this->assertEquals($expected, $condition->getCondition());
+	}
+	
+	public function testConditionIsConstantScoreIsTrue () {
+		$expected = true;
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition->setConstantScore();
+		$this->assertEquals($expected, $condition->isConstantScore());
+	}
+	
+	public function testConditionIsNegatedIsTrue () {
+		$expected = true;
+		$condition = new Condition('acceptedName.specificEpithet', 'MATCHES', 'fuscus');
+		$condition->negate();
+		$this->assertEquals($expected, $condition->isNegated());
+	}
+	
+	public function testConditionWithIncorrectlyConstructedOperator () {
 		$e = new \stdClass();
 		try {
 			$condition = new Condition('acceptedName.genusOrMonomial', 'AMTCHES', 'larus');;
@@ -119,15 +178,28 @@ class ConditionTest extends \PHPUnit_Framework_TestCase
 		$this->assertEquals('UnexpectedValueException', get_class($e));
 	}
 	
-	public function testConditionWithIncorrectlyConstructedEmptyValue ()
-	{
+	public function testConditionWithIncorrectlyConstructedEmptyValue () {
 		$e = new \stdClass();
 		try {
-			$condition = new Condition('acceptedName.genusOrMonomial', 'NOT_LIKE');;
+			$condition = new Condition('acceptedName.genusOrMonomial', 'NOT_LIKE');
 		} catch (\Exception $e) {}
 		$this->assertEquals('InvalidArgumentException', get_class($e));
 	}
 	
-	
+	public function testSetCorrectBoost () {
+		$expected = 1.3;
+		$condition = new Condition('acceptedName.genusOrMonomial', 'EQUALS_IC', 'larus');
+		$condition->setBoost(1.3);
+		$this->assertEquals($expected, $condition->getBoost());
+	}
+
+	public function testSetIncorrectBoost () {
+		$e = new \stdClass();
+		try {
+			$condition = new Condition('acceptedName.genusOrMonomial', 'NOT_LIKE');
+			$condition->setBoost('a lot');
+		} catch (\Exception $e) {}
+		$this->assertEquals('InvalidArgumentException', get_class($e));
+	}
 	
 }
