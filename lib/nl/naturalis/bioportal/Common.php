e117306feef6e83b4b5bf1d0c66a7684fb1ccd99
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-03-13 11:32:13 +0100

    phpdoc

--- lib/nl/naturalis/bioportal/Common.php
+++ lib/nl/naturalis/bioportal/Common.php
@@ -6,6 +6,13 @@
         public function __construct () {
         }
 
+        /**
+         * Valid NBA operators
+         * 
+         * Can be used to validate a given operator.
+         * 
+         * @var array Valid NBA operators
+         */
 		public static $operators = [
             'BETWEEN',
             'EQUALS',
@@ -25,16 +32,39 @@
 		    'NOT_MATCHES',
 		];
 
+		/**
+		 * Valid NBA logical operators
+		 *
+		 * Can be used to validate a given logical operator.
+		 *
+		 * @var array Valid NBA logical operators
+		 */
 		public static $logicalOperators = [
             'AND',
 		    'OR',
 		];
 
+		/**
+		 * Valid NBA sort directions
+		 *
+		 * Can be used to validate a given sort direction.
+		 *
+		 * @var array Valid NBA sort directions
+		 */
 		public static $sortDirections = [
 		    'ASC',
 		    'DESC',
 		];
 
+        /**
+         * Validate integer input
+         * 
+         * Can be used to check if input is integer or string that
+         * can be safely cast to integer (25 vs "25").
+         * 
+         * @param unknown $i
+         * @return boolean
+         */
         public function isInteger ($i) {
             if (ctype_digit(strval($i))) {
                 return true;
@@ -43,16 +73,22 @@
         }
 
         /**
-         * Input: string_with_underscores, output: stringWithUnderscores
+         * Convert string with underscores to camelCased version
+         * 
+         * @example this_is_a_string to thisIsAString
+         * @param unknown $str
+         * @return string
          */
         public function camelCase ($str) {
             return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $str))));
         }
 
-    	 /**
-		 * Takes either an array or (comma-separated) string;
-		 * trims spaces and returns object with string
-		 */
+        /**
+         * Cleans input string with commas or converts array to comma-separated string
+         * 
+         * @param string|array $data
+         * @return string Comma-separated string
+         */
 		 public function commaSeparate ($data = '') {
             if (!is_array($data)) {
                 $p = array_map('trim', explode(',', $data));
