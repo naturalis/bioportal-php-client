3df24bea7b3a4a30c121bda171416ddd97d32f62
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-03-22 13:23:57 +0100

    nog een keer

--- example/names.php
+++ example/names.php
@@ -9,6 +9,7 @@
 	require_once '../lib/nl/naturalis/bioportal/Client.php';
 	require_once '../lib/nl/naturalis/bioportal/Condition.php';
 	require_once '../lib/nl/naturalis/bioportal/QuerySpec.php';
+	require_once '../lib/nl/naturalis/bioportal/NameGroupQuerySpec.php';
 	
 	// Initialise Client
 	$client = new Client();
