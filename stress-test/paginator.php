4635d724b1b84d99a5c403a0803db875e1eaa360
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-07-03 09:12:20 +0200

    paginator

--- stress-test/paginator.php
+++ stress-test/paginator.php
@@ -9,6 +9,9 @@
     // Initialise Client
     $client = new Client();
     
+    // Running time (in mins); set to 1 for just one loop
+    $runningTime = 120;
+    
     // Loop size
     $size = 10000;
     
@@ -24,54 +27,68 @@
     $max = $client
     	->specimen()
     	->getIndexMaxResultWindow();
+    
+    $scriptStart = microtime(true);
+    
+    // Time based loop
+    if ((microtime(true) - $scriptStart) < ($runningTime * 60)) {
+    	
+    	$stats = [];
 
-	for ($i = 0; $i < $max; $i += $size) {
-		
-		$condition = new Condition('sourceSystem.code', 'EQUALS', 'BRAMHS');
-	    $query = new QuerySpec();
-	    $query
-	    	->addCondition($condition)
-	    	->setSize($size)
-	    	->setFrom($i);
-
-	   	// Start loop timer
-	    $loopStart = microtime(true);
-	    
-	    // Do query
-	    $client
-	    	->specimen()
-	    	->setQuerySpec($query)
-	    	->query();
-	   	
-	   	// End loop timer
-	    $loopEnd = round(microtime(true) - $loopStart, 2);
-	    	
-	    if ($i > 0 && $i % ($size * $printLoops) == 0) {
-	    	
-	    	// Get stats for loop
-	    	$currentStats = array_slice($stats, $i / $size - $printLoops, $i / $size);
-	    	
-	    	// Print stats
-	    	echo 'Records ' . ($i - ($size * $printLoops) + 1) . " to $i\n";
-	    	echo 'min: ' . min($currentStats) . "s\tmax: " . max($currentStats) . "s\taverage: " .
-	    		round(array_sum($currentStats) / count($currentStats), 2) . "s\n\n";
-	    }
-	    
-	    // Store to calculate average query time
-	    $stats[$i] = $loopEnd;
-	    
-	    $lastCounter = $i;
-	    
-	}
+		// Result-based loop
+		for ($i = 0; $i < $max; $i += $size) {
+			
+			$condition = new Condition('sourceSystem.code', 'EQUALS', 'BRAMHS');
+		    $query = new QuerySpec();
+		    $query
+		    	->addCondition($condition)
+		    	->setSize($size)
+		    	->setFrom($i);
 	
-	// Test if there's a last set to print
-	if ($lastCounter < $i) {
+		   	// Start loop timer
+		    $loopStart = microtime(true);
+		    
+		    // Do query
+		    $client
+		    	->specimen()
+		    	->setQuerySpec($query)
+		    	->query();
+		   	
+		   	// End loop timer
+		    $loopEnd = round(microtime(true) - $loopStart, 2);
+		    	
+		    if ($i > 0 && $i % ($size * $printLoops) == 0) {
+		    	
+		    	// Get stats for loop
+		    	$currentStats = array_slice($stats, $i / $size - $printLoops, $i / $size);
+		    	
+		    	// Print stats
+		    	echo 'Records ' . ($i - ($size * $printLoops) + 1) . " to $i\n";
+		    	echo 'min: ' . min($currentStats) . "s\tmax: " . max($currentStats) . "s\taverage: " .
+		    		round(array_sum($currentStats) / count($currentStats), 2) . "s\n\n";
+		    }
+		    
+		    // Store to calculate average query time
+		    $stats[$i] = $loopEnd;
+		    
+		    $lastCounter = $i;
+		    
+		}
 		
-		$lastStats = array_slice($stats, array_search($lastCounter, array_keys($stats)), count($i));
-	    
-	    // Print stats
-	    echo 'Records ' . ($i - ($size * $printLoops) + 1) . " to $i\n";
-	    echo 'min: ' . min($lastStats) . "s\tmax: " . max($lastStats) . "s\taverage: " .
-	    	round(array_sum($lastStats) / count($lastStats), 2) . "s\n\n";
-	}
+		// Test if there's a last set to print
+		if ($lastCounter < $i) {
+			
+			$lastStats = array_slice($stats, array_search($lastCounter, array_keys($stats)), count($i));
+		    
+		    // Print stats
+		    echo 'Records ' . ($i - ($size * $printLoops) + 1) . " to $i\n";
+		    echo 'min: ' . min($lastStats) . "s\tmax: " . max($lastStats) . "s\taverage: " .
+		    	round(array_sum($lastStats) / count($lastStats), 2) . "s\n\n";
+		}
+		
+		echo 'Sum of loop times: ' . array_sum($stats) . "s\n";
+		echo 'Script running time: ' . round((microtime(true) - $scriptStart) / 60) . "m\s\s\s";
+	
+    }
+	
     
